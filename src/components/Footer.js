de21b0c26757ff61ac1e1d73132eb7f484ca58f8
Alexey <shumil.alexey@gmail.com>
2021-02-04 16:51:25 +0200

    first
    mesto-react(11 project work)

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="footer page__footer">
+      <p className="footer__title">© 2020 Mesto Russia</p>
+    </footer>
+  );
+};
+
+export default Footer;
